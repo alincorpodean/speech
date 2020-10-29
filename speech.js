18eeabd626ce4677607e269ac454f86a0b9bcc1f
alincorpodean <72561107+alincorpodean@users.noreply.github.com>
2020-10-29 16:30:09 +0200

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,30 @@
+document.addEventListener("touchstart",on_touch);
+document.addEventListener("mousedown",on_touch);
+
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'en-US';
+
+function on_touch()
+{
+	if(recognition.start)
+	{
+		recognition.start();
+		recognition_started = true;
+	}
+}
+
+function onend()
+{
+	recognition.stop();
+	recognition_started = false;
+}
+
+recognition.onend = onend;
+recognition.onsoundend = onend;
+recognition.onspeechend = onend;
+recognition.onresult = on_result;
+
+function on_result(e)
+{
+	document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ e.results[0][0].transcript +  ", acuratete:"+e.on_results[0][0].confidence + "<br>"
+}
